2d4f09dfaec4b8adf4eccc31471ba0fac8e04eea
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-25 12:00:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useState,useRef, useEffect } from 'react';
+import RefFocus from './components/RefFocus';
+import PreviousValue from './components/PreviousValue';
+
+//useRef hook when we want to see how many times our screen renders
+//it persists trough multiple renders of our component
+// ref does not cause component to re update when a certain state changes
+//when it updates the current property persists through the multiple renders
+//you can store previous values but it won't cause items to re-render
+// to store previous value of state
 
 function App() {
+  const [name,setName] = useState(' ')
+  const renderCount = useRef(0) // returns an object {current: 0}
+
+  function handleChange(e){
+    setName(e.target.value)
+  }
+
+  useEffect(()=>{
+    renderCount.current = renderCount.current + 1 // here we are updating the value by one
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={name} onChange={handleChange}/>
+      <div>My name is {name}</div>
+      <div>I rendered {renderCount.current} times</div>
+      <br/>
+      <RefFocus/>
+      <br/>
+      <PreviousValue/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/PreviousValue.js
@@ -0,0 +1,25 @@
+import React, { useEffect,useState,useRef } from "react";
+
+function PreviousValue(){ // to persist previous values of state by persisting the previous value
+    const [name, setName]= useState('')
+    const previousName=useRef('')
+
+    useEffect(()=>{
+        previousName.current = name;
+    },[name]);
+
+    function handleChange(e){
+        setName(e.target.value)
+    }
+
+    return(
+        <div>
+            <input value={name} onChange={handleChange}/>
+            <div>My name is {name} and my previous name is {previousName.current}</div>
+        </div>
+    )
+
+
+}
+
+export default PreviousValue;
